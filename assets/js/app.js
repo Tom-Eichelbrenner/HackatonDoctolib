65f630a8655b998a0acbc50b6b4fad8c5c4a1921
Mirubrys <mic.mendesdos@gmail.com>
2020-06-25 21:27:01 +0200

    Added bootstrap as requirement for webpack.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+require('bootstrap');
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/app.scss';
 
 // Importing Bootstrap
 const $ = require('jquery');
-require('bootstrap');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
